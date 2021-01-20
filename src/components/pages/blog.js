4f105d6e201fd2a92db46e3dc5ae496eaa84b165
Bridger Larsen <bridgerjimmy@gmail.com>
2021-01-20 15:18:39 -0700

    called_blog_api_rendered_blog_records_to_screen

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-export default function() {
-    return (
-        <div>
-            <h2>Blog</h2>
+class Blog extends Component {
+    constructor() {
+        super();
 
+        this.state = {
+            BlogItems: []
+        };
+
+        this.getBlogItems = this.getBlogItems.bind(this);
+    }
+
+    getBlogItems() {
+        axios.get("https://bridgerlarsen.devcamp.space/portfolio/portfolio_blogs", {
+            withCredentials: true
+        })
+        .then(response => {
+            this.setState({
+                BlogItems: response.data.portfolio_blogs
+            })
+        })
+        .catch(error => {
+            console.log("getBlogItems error", error);
+        })
+    }
+
+    componentWillMount() {
+        this.getBlogItems();
+    }
+
+    render() {
+        const blogRecords = this.state.BlogItems.map(blogItem => {
+            return <h1>{blogItem.title}</h1>
+        })
+        return (
             <div>
-                <Link to='/about-me'>
-                    Read more about myself
-                </Link>
+                {blogRecords}
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default Blog;
