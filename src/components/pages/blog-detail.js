2456efe12b00512143288f1e3cdc1140df73192a
Bridger Larsen <bridgerjimmy@gmail.com>
2021-01-22 13:11:53 -0700

    Built_initail_blog_detail_component

---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class BLogDetail extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi There</h1>
+            </div>
+        )
+    }
+}
