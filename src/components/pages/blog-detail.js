753cd43c5924016603b003d2ee5734d2d0e24972
Bridger Larsen <bridgerjimmy@gmail.com>
2021-01-22 14:17:02 -0700

    grabbed_blogItem_from_api_then_rendered_blog_detail_toScreen

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class BLogDetail extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentId: this.props.match.params.slug,
+            blogItem: {}
+        };
+    }
+
+    getBlogItem() {
+        axios.get(
+            `https://bridgerlarsen.devcamp.space/portfolio/portfolio_blogs/
+            ${this.state.currentId}`
+        )
+        .then(response => {
+            this.setState({
+                blogItem: response.data.portfolio_blog
+            });
+        })
+        .catch(error => {
+            console.log("getBlogItem error", error);
+        })
+    }
+
+    componentDidMount() {
+        this.getBlogItem();
+    }
+
     render() {
+        const {
+            title,
+            content,
+            featured_image_url,
+            blog_status
+        } = this.state.blogItem;
+
         return (
             <div>
-                <h1>Hi There</h1>
+                <h1>{title}</h1>
+                <img src={featured_image_url} />
+                <div>{content}</div>
             </div>
         )
     }
