29ba1c06efd16479119420520a36071265bf8013
Bridger Larsen <bridgerjimmy@gmail.com>
2021-01-27 15:54:43 -0700

    fixed_screen_reading_issue_for_modal_created_blogForm_component

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import ReactModal from 'react-modal';
 
+import BlogForm from '../blog/blog-form';
+
+ReactModal.setAppElement(".app-wrapper");
+
 export default class BlogModal extends Component {
     constructor() {
         super();
+
+        this.customStyles = {
+            content: {
+                top: "50%",
+                left: "50%",
+                right: "auto",
+                marginRight: "-50%",
+                transform: "translate(-50%, -50%)",
+                width: "800px"
+            },
+            overlay: {
+                backgroundColor: "rgba(1, 1, 1, 0.75)"
+            }
+        }
     }
 
     render() {
         return (
             <ReactModal 
+                style={this.customStyles}
                 onRequestClose={() => {
                 this.props.handleModalClose();
             }} isOpen={this.props.modalIsOpen}
             >
-                <h1>Hi, I'm in a modal!</h1>
+                <BlogForm />
             </ReactModal>
         )
     }
