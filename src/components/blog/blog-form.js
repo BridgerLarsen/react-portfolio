2fcf284deaefeddac909e508fff33da278df2c05
Bridger Larsen <bridgerjimmy@gmail.com>
2021-01-27 17:10:26 -0700

    added_blog_form_event_handlers

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -3,15 +3,47 @@ import React, { Component } from 'react';
 export default class BlogForm extends Component {
     constructor() {
         super();
+
+        this.state = {
+            title: "",
+            blog_status: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        this.props.handleSuccessfullFormSubmission(this.state);
+        event.preventDefault();
     }
 
     render() {
         return (
-            <form>
-                <input type="text" />
-                <input type="text" />
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    type="text" 
+                    onChange={this.handleChange}
+                    name="title"
+                    placeholder="Blog Title"
+                    value={this.state.title}
+                />
 
-                <button>Save</button>
+                <input 
+                    type="text" 
+                    onChange={this.handleChange}
+                    name="blog_status"
+                    placeholder="Blog Status"
+                    value={this.state.blog_status}
+                />
+
+                <button type="submit">Save</button>
             </form>
         )
     }
