c39c71dab3341d3591fbb40b81cd5454083101c7
Bridger Larsen <bridgerjimmy@gmail.com>
2021-01-27 17:53:54 -0700

    creating_blog_post_on_form_modal_through_api

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class BlogForm extends Component {
     constructor() {
@@ -13,6 +14,15 @@ export default class BlogForm extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    buildForm() {
+        let formData = new FormData();
+
+        formData.append("portfolio_blog[title]", this.state.title);
+        formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+
+        return formData;
+    }
+
     handleChange(event) {
         this.setState({
             [event.target.name]: event.target.value
@@ -20,7 +30,18 @@ export default class BlogForm extends Component {
     }
 
     handleSubmit(event) {
-        this.props.handleSuccessfullFormSubmission(this.state);
+        axios.post(
+            "https://bridgerlarsen.devcamp.space/portfolio/portfolio_blogs",
+            this.buildForm(),
+            { withCredentials: true }
+        )
+        .then(response => {
+            this.props.handleSuccessfullFormSubmission(response.data);
+        })
+        .catch(error => {
+            console.log("handleSubmit blog error", error);
+        })
+
         event.preventDefault();
     }
 
